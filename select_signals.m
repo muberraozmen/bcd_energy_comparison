dd74e35a6a6b8d1668f1f910594ffec85bd36b84
muberra <muberra@muberra-XPS-8930>
2019-10-26 19:54:22 -0400

    mean with selected antenna pairs

--- select_signals.m
+++ select_signals.m
@@ -1,4 +1,4 @@
-function select_signals (reference_folder, windowing)
+function select_signals (reference_folder, windowing, filtering)
 
 reference_folder = '/home/muberra/Documents/MATLAB/bcd_energy_comparison/data/reference_scans';
 % Info: Reference folder contains a number of Baseline and Tumour scans 
@@ -29,28 +29,36 @@ end
 clear temp_folders i ;
 
 %% Compare scans 
+% Note: we use a matrix this time instead of cell (different than
+% population generation)
+
+num_signals = size(pool_baseline{1},2);
 
 % pairwise comparisons between baseline scans 
-B2B = cell(num_baseline, num_baseline);
+B2B = zeros((num_baseline-2)*(num_baseline-1), num_signals);
+counter = 1;
 for i = 1:num_baseline
     for j = (i+1):num_baseline
-        B2B{i,j} = compare_scans(pool_baseline{i}, pool_baseline{j}, windowing); 
+        B2B(counter,:) = compare_scans(pool_baseline{i}, pool_baseline{j}, windowing); 
+        counter = counter +1;
     end
 end
 
 % pairwise comparisons between baseline scans and tumour scans 
-B2T = cell(num_baseline, num_tumour);
+B2T = zeros(num_baseline*num_tumour, num_signals);
+counter = 1;
 for i = 1:num_baseline
     for j = 1:num_tumour
-        B2T{i,j} = compare_scans(pool_baseline{i}, pool_tumour{j}, windowing); 
+        B2T(counter,:) = compare_scans(pool_baseline{i}, pool_tumour{j}, windowing); 
+        counter = counter +1;
     end
 end
 
-clear i j;
+clear i j counter;
 
-%% Energy 
-
-
+%% Sort signals to have high B2T difference but low B2B difference
+% Note: we may use scaling and weights
+evaluated_criteria = sum(B2T,1) - sum(B2B,1);
 
 
 end
