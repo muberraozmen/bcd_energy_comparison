75a336f388d516b8a4a0d172101aa2266a086f68
muberra <muberra@muberra-XPS-8930>
2019-10-27 15:32:59 -0400

    antenna pairs selection update

--- select_signals.m
+++ select_signals.m
@@ -56,9 +56,30 @@ end
 
 clear i j counter;
 
+%% Find antenna pair indices 
+scan_dir = '/home/muberra/Documents/MATLAB/bcd_energy_comparison/data/reference_scans/Baseline1';
+
+temp_files = struct2table(dir(scan_dir));
+
+% delete system files
+to_delete = ~(contains(temp_files.name, '.txt') & startsWith(temp_files.name, 'sig'));
+temp_files(to_delete,:) = [];  
+
+scan_files = sortrows(temp_files,'name'); % sort by file name (i.e. antenna pairs as text)
+clear temp_files to_delete;
+
+antenna_pair_indices = strings(height(scan_files),2);
+for i = 1:height(scan_files)
+    temp = strsplit(char(scan_files{i,'name'}), {'A','_'});
+    antenna_pair_indices(i,1) = temp(2);
+    antenna_pair_indices(i,2) = temp(3);
+end
+
 %% Sort signals to have high B2T difference but low B2B difference
 % Note: we may use scaling and weights
 evaluated_criteria = sum(B2T,1) - sum(B2B,1);
+[~, I] = maxk(evaluated_criteria, 20);
+antenna_pair_indices(I,:)
 
 
 end
